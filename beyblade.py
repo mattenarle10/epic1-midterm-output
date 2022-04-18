36cfd000c58c225c0a1929b7f7c2b99a359d2509
mattenarle10 <89822774+mattenarle10@users.noreply.github.com>
2022-04-18 14:16:46 +0800

    Add files via upload

---
+++ beyblade.py
@@ -0,0 +1,50 @@
+#Matthew Enarle BSCS 1-B
+from turtle import *
+
+bgcolor("black")
+speed(3000)
+
+def drawRasengan():
+    colors = ['white', 'lightblue', 'blue', 'lightgray']
+    for line in range(180):
+        penup()
+        goto(0,0)
+        pendown()
+        left(1)
+        for curve in range(4):
+            if curve == 0:
+                color( colors[0])
+            elif curve == 1:
+                color( colors[1])
+            elif curve == 2:
+                color( colors[2])
+            elif curve == 3:
+                color( colors[3])
+            forward(50)
+            left(50)
+
+def drawShuriken():
+    color("red")
+    penup()
+    goto(100,92)
+    pendown()
+    for i in range(100):
+        left(120)
+        forward(i)
+        backward(i%800)
+        left(145)
+        forward(200)
+
+drawShuriken()
+drawRasengan()
+
+hideturtle()
+done()
+
+
+#shuriken source: https://www.youtube.com/watch?v=vAZ5CycYDtE
+#modified his design wherein the shuriken acts as the spikes of a beyblade, more specifically, edited the loop iteration and radius and the number of spikes
+
+#rasengan source: https://replit.com/@Roger_Lai/mega-rasengan
+#changed the colors of the rasengan to make it seem like the body of the blade
+
---
+++ cyclone.py
@@ -0,0 +1,50 @@
+#Matthew Enarle BSCS 1-B
+from turtle import *
+title("Mandala Pattern - Enarle")
+
+bgcolor("black")
+speed(3000)
+
+def drawRasengan():
+    colors = ['white', 'lightblue', 'blue', 'lightgray']
+    for line in range(180):
+        penup()
+        goto(0,0)
+        pendown()
+        left(1)
+        for curve in range(4):
+            if curve == 0:
+                color( colors[0])
+            elif curve == 1:
+                color( colors[1])
+            elif curve == 2:
+                color( colors[2])
+            elif curve == 3:
+                color( colors[3])
+            forward(50)
+            left(50)
+
+
+def drawShuriken():
+    color("white")
+    penup()
+    goto(-50,-10)
+    pendown()
+    for i in range(300):
+        left(60)
+        forward(i)
+        backward(i%800)
+        left(145)
+        forward(100)
+
+drawRasengan()
+drawShuriken()
+hideturtle()
+done()
+
+#shuriken source: https://www.youtube.com/watch?v=vAZ5CycYDtE
+#edited the shuriken to another level which makes it larger but not so sharper blades to make it seem like a cyclone
+#edited the radius of the loop and its position to match perfectly with rasengan
+
+#rasengan source: https://replit.com/@Roger_Lai/mega-rasengan
+#changed the colors of the rasengan to make it looke like a cyclone, and also edited the range so that the itiretation will be less and simple
